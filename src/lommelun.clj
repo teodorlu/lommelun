c0838ca9d72c4968b98f8b85ca9b332baeb46f37
Teodor Heggelund <git@teod.eu>
2025-04-03 13:02:53 +0200

    Skriv HTML-serveren

--- src/lommelun.clj
+++ src/lommelun.clj
@@ -1,8 +1,28 @@
 (ns lommelun
   (:require [org.httpkit.server :as server]))
 
-(defn handler [http-request]
-  {:status 200 :body "LOL"})
+(defn html-en []
+  {:status 200
+   :headers {"Content-Type" "text/html; charset=utf-8"}
+   :body (slurp "index.html")})
+
+(defn javascript-en []
+  {:status 200
+   :headers {"Content-Type" "text/javascript; charset=utf-8"}
+   :body (slurp "lommelun.js")})
+
+(defn handler [{:keys [request-method uri]}]
+  (cond
+    (and (= request-method :get)
+         (= uri "/"))
+    (html-en)
+
+    (and (= request-method :get)
+         (= uri "/lommelun.js"))
+    (javascript-en)
+
+    :else
+    {:status 200 :body "LOL"}))
 
 (defn start! [_]
-  (server/run-server handler {:port 7777 :legacy-return-value? false}))
+  (server/run-server #'handler {:port 7777 :legacy-return-value? false}))
